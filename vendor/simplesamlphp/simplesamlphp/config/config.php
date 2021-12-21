b731ddc59b86877d2ad85cd40f33cd4bb849f5f9
chikhalifa <chikhalifa23@yahoo.com>
2021-12-21 01:43:49 +0100

    fixed installation issues

--- vendor/simplesamlphp/simplesamlphp/config/config.php
+++ vendor/simplesamlphp/simplesamlphp/config/config.php
@@ -566,7 +566,7 @@ $config = [
      * through https. If the user can access the service through
      * both http and https, this must be set to FALSE.
      */
-    'session.cookie.secure' => true,
+    'session.cookie.secure' => FALSE,
 
     /*
      * Set the SameSite attribute in the cookie.
